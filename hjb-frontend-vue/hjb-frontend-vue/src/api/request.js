67f35f3db38fce92d391259c45b459f9d683dfb1
hhl <hl6275@nyu.edu>
2026-04-08 11:44:06 -0400

    配置生产环境：前端baseURL改为相对路径，后端数据库改为环境变量

--- hjb-frontend-vue/hjb-frontend-vue/src/api/request.js
+++ hjb-frontend-vue/hjb-frontend-vue/src/api/request.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import router from '../router/index.js'
 
 const request = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: '/api',
   timeout: 5000
 })
 
